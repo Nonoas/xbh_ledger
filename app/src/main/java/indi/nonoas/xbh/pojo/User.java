f4be0162ae647edbba06f108d8e5625926149112
Nonoas <1356955215@qq.com>
2021-09-09 21:54:28 +0800

    初始化

---
+++ app/src/main/java/indi/nonoas/xbh/pojo/User.java
@@ -0,0 +1,34 @@
+package indi.nonoas.xbh.pojo;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
+@Entity
+public class User {
+
+    @Id
+    private long id;
+    private String name;
+    @Generated(hash = 1144922831)
+    public User(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    @Generated(hash = 586692638)
+    public User() {
+    }
+    public long getId() {
+        return this.id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
